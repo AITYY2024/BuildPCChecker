4029a4727796a89f39fe78f6aad0c7fe83ad9818
aniya <24aniya_io@axiz.co.jp>
2024-06-12 16:11:51 +0900

    初期化済み

---
+++ src/main/java/com/buildpcchecker/buildpcchecker/BuildpccheckerApplication.java
@@ -0,0 +1,13 @@
+package com.buildpcchecker.buildpcchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildpccheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildpccheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buildpcchecker/buildpcchecker/BuildpccheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildpcchecker.buildpcchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildpccheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
