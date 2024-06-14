319a696975606f06cfd7c48d23ef5b2b7a4d9198
aniya <24aniya_io@axiz.co.jp>
2024-06-14 12:48:09 +0900

    テキストをコピペした状態

---
+++ src/main/resources/static/js/test-insert.js
@@ -0,0 +1,21 @@
+'use strict'
+    // ボタンクリック時の処理を定義
+    document.getElementById('btn').addEventListener('click', () => {
+        // リクエストで送信するJSON用のオブジェクト作成
+        const user = {
+//          Javaのフォームクラスのフィールド名:document.getElementById('HTMLのID').value
+            name: document.getElementById('name').value,
+            mail: document.getElementById('mail').value,
+            age: document.getElementById('age').value,
+        }
+        // POSTでリクエスト送信
+        // userをJSONに変換して送る
+        fetch('/api/user', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(user),
+        })
+        .then(res => console.log(res))
+    })
