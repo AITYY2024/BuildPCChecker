5c076646e4e43e7ec27dd7ca775b761227ec58dd
tairahoshito <yamada@xxx.com>
2024-06-17 15:37:20 +0900

    git warui

--- src/main/java/com/buildpcchecker/buildpcchecker/form/MbSelectForm.java
+++ src/main/java/com/buildpcchecker/buildpcchecker/form/MbSelectForm.java
@@ -1,14 +1,16 @@
 package com.buildpcchecker.buildpcchecker.form;
 
-import java.util.Date;
+import lombok.Data;
 
+import java.util.Date;
+@Data
 public class MbSelectForm {
 
     private int id;
     private String product_id;
     private String url;
     private String product_name;
-    private int price;
+    private Integer price;
     private Date release_date;
     private String formfactor;
     private String chipset;
--- src/main/java/com/buildpcchecker/buildpcchecker/parts_select/ProductsDao.java
+++ src/main/java/com/buildpcchecker/buildpcchecker/parts_select/ProductsDao.java
@@ -16,6 +16,7 @@ public class ProductsDao implements IProductsDao{
     @Autowired
     private NamedParameterJdbcTemplate jdbcTemplate;
 
+    //選んだカテゴリーのパーツを全て表示する
     //CPUのテーブルを全て表示する
     @Override
     public List<CpuSelectForm> tableCpuParts(){
@@ -47,12 +48,12 @@ public class ProductsDao implements IProductsDao{
                 """,new DataClassRowMapper<>(MbSelectForm.class));
     }
 
-    //Storageのテーブルを全て表示する
+    //SSDのテーブルを全て表示する
     @Override
-    public List<StorageSelectForm> tableStorageParts(){
+    public List<SsdSelectForm> tableSsdParts(){
         return jdbcTemplate.query("""
-                SELECT * FROM storage
-                """,new DataClassRowMapper<>(StorageSelectForm.class));
+                SELECT * FROM ssd
+                """,new DataClassRowMapper<>(SsdSelectForm.class));
     }
 
     //PSUのテーブルを全て表示する
@@ -79,7 +80,73 @@ public class ProductsDao implements IProductsDao{
             param.addValue("cpu_name", "%" + cpu_name + "%");
             var list = jdbcTemplate.query("""
                 SELECT * FROM cpu
-                WHERE product_name = :cpu_name""", param, new DataClassRowMapper<>(CpuSelectForm.class));
+                WHERE product_name LIKE :cpu_name""", param, new DataClassRowMapper<>(CpuSelectForm.class));
+            return list;
+    }
+
+    //GPUのテーブルから検索したものを表示する
+    @Override
+    public List<GpuSelectForm> searchGpuParts(String gpu_name){
+            var param = new MapSqlParameterSource();
+            param.addValue("gpu_name", "%" + gpu_name + "%");
+            var list = jdbcTemplate.query("""
+                SELECT * FROM gpu
+                WHERE product_name LIKE :gpu_name""", param, new DataClassRowMapper<>(GpuSelectForm.class));
+            return list;
+    }
+
+    //Memoryのテーブルから検索したものを表示する
+    @Override
+    public List<MemorySelectForm> searchMemoryParts(String memory_name){
+            var param = new MapSqlParameterSource();
+            param.addValue("memory_name", "%" + memory_name + "%");
+            var list = jdbcTemplate.query("""
+                SELECT * FROM memory
+                WHERE product_name LIKE :memory_name""", param, new DataClassRowMapper<>(MemorySelectForm.class));
+            return list;
+    }
+
+    //Mbのテーブルから検索したものを表示する
+    @Override
+    public List<MbSelectForm> searchMbParts(String mb_name){
+            var param = new MapSqlParameterSource();
+            param.addValue("mb_name", "%" + mb_name + "%");
+            var list = jdbcTemplate.query("""
+                SELECT * FROM mb
+                WHERE product_name LIKE :mb_name""", param, new DataClassRowMapper<>(MbSelectForm.class));
+            return list;
+    }
+
+    //SSDのテーブルから検索したものを表示する
+    @Override
+    public List<SsdSelectForm> searchSsdParts(String ssd_name){
+            var param = new MapSqlParameterSource();
+            param.addValue("ssd_name", "%" + ssd_name + "%");
+            var list = jdbcTemplate.query("""
+                SELECT * FROM ssd
+                WHERE product_name LIKE :ssd_name""", param, new DataClassRowMapper<>(SsdSelectForm.class));
+            return list;
+    }
+
+    //PSUのテーブルから検索したものを表示する
+    @Override
+    public List<PsuSelectForm> searchPsuParts(String psu_name){
+            var param = new MapSqlParameterSource();
+            param.addValue("psu_name", "%" + psu_name + "%");
+            var list = jdbcTemplate.query("""
+                SELECT * FROM psu
+                WHERE product_name LIKE :psu_name""", param, new DataClassRowMapper<>(PsuSelectForm.class));
+            return list;
+    }
+
+    //OSのテーブルから検索したものを表示する
+    @Override
+    public List<OsSelectForm> searchOsParts(String os_name){
+            var param = new MapSqlParameterSource();
+            param.addValue("os_name", "%" + os_name + "%");
+            var list = jdbcTemplate.query("""
+                SELECT * FROM os
+                WHERE product_name LIKE :os_name""", param, new DataClassRowMapper<>(OsSelectForm.class));
             return list;
     }
 
