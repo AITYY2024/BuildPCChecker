1594aaacbacc4c4241c09b6bdcf288bfc14b2666
isa <24isa_kota@axiz.co.jp>
2024-06-17 17:11:57 +0900

    6/17(月)17:11更新

--- src/main/java/com/buildpcchecker/buildpcchecker/BuildpccheckerApplication.java
+++ src/main/java/com/buildpcchecker/buildpcchecker/BuildpccheckerApplication.java
@@ -1,12 +1,7 @@
 package com.buildpcchecker.buildpcchecker;
 
-import com.buildpcchecker.buildpcchecker.form.PresetListForm;
-import com.buildpcchecker.buildpcchecker.preset.PresetDao;
-import com.buildpcchecker.buildpcchecker.preset.PresetService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 
 @SpringBootApplication
 public class BuildpccheckerApplication {
@@ -14,5 +9,4 @@ public class BuildpccheckerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BuildpccheckerApplication.class, args);
 	}
-
 }
