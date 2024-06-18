0565497ca65cd0fca351ec4688e97fdde54d5500
aniya <24aniya_io@axiz.co.jp>
2024-06-18 14:14:37 +0900

    compatibleのテストページ追加

--- src/main/java/com/buildpcchecker/buildpcchecker/BuildpccheckerApplication.java
+++ src/main/java/com/buildpcchecker/buildpcchecker/BuildpccheckerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BuildpccheckerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BuildpccheckerApplication.class, args);
 	}
