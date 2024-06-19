674f46cc139bde1869919062bf1e48ac56e14dac
isa <24isa_kota@axiz.co.jp>
2024-06-19 15:16:52 +0900

    6/19(水)更新2

--- src/main/java/com/buildpcchecker/buildpcchecker/preset/IPresetDao.java
+++ src/main/java/com/buildpcchecker/buildpcchecker/preset/IPresetDao.java
@@ -5,7 +5,12 @@ import com.buildpcchecker.buildpcchecker.form.PresetListForm;
 import java.util.List;
 
 public interface IPresetDao {
-    List<PresetListForm> findAll();//プリセットテーブル一覧
+
+    List<PresetListForm> findAll(Integer user_id);//プリセットテーブル一覧
+
+    PresetListForm findById(Integer preset_id);//プリセット詳細表示
 
     Integer delete(Integer preset_id);//プリセットテーブル削除
+
+    Integer update(PresetListForm presetListForm);//プリセット編集
 }
--- src/main/java/com/buildpcchecker/buildpcchecker/preset/PresetRestController.java
+++ src/main/java/com/buildpcchecker/buildpcchecker/preset/PresetRestController.java
@@ -4,10 +4,7 @@ import com.buildpcchecker.buildpcchecker.form.PresetListForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.List;
@@ -19,18 +16,58 @@ public class PresetRestController {
     PresetService presetService;
 
     //プリセット一覧表示用
-    @GetMapping("/test")
-    public ResponseEntity<List<PresetListForm>> PresetList() {
+    @GetMapping("/api/product")
+    public ResponseEntity<List<PresetListForm>> PresetList(Integer user_id) {
         try {
-            List<PresetListForm> presetList = presetService.findAll();
+            List<PresetListForm> presetList = presetService.findAll(1);
             // データとステータスコード200番を返す
             return new ResponseEntity<>(presetList, HttpStatus.OK);
         } catch (RuntimeException e) {
             // ステータスコード400番を返す
-            e.printStackTrace();//エラー内容をコンソール画面に表示するための処理
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
+
+//    //プリセット詳細表示
+//    @GetMapping("/api/productDetail")
+//    public ResponseEntity<PresetListForm> presetDetail(Integer preset_id) {
+//        try {
+//            PresetListForm presetDetails = presetService.findById(2);
+//            // データとステータスコード200番を返す
+//            return new ResponseEntity<>(presetDetails, HttpStatus.OK);
+//        } catch (RuntimeException e) {
+//            // ステータスコード400番を返す
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        }
+//    }
+
+//    //プリセット削除用
+//    @DeleteMapping(value = "/api/productDelete")
+//    public ResponseEntity<Integer> delete(@RequestBody PresetListForm presetListForm) {
+//        try {
+//            Integer deletedPreset = presetService.delete(1);
+//            // データとステータスコード200番を返す
+//            return new ResponseEntity<>(deletedPreset, HttpStatus.OK);
+//        } catch (RuntimeException e) {
+//            // ステータスコード400番を返す
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        }
+//    }
+
+//    //プリセット編集用
+//    @PutMapping(value = "/api/")
+//    public ResponseEntity inputUser(@RequestBody ) {
+//        session.setAttribute("sessionUser", users);
+//        try {
+////            Integer deletedPreset = presetService.delete(1);
+////            // データとステータスコード200番を返す
+////            return new ResponseEntity<>(deletedPreset, HttpStatus.OK);
+////        } catch (RuntimeException e) {
+////            // ステータスコード400番を返す
+////            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+////        }
+//        return ResponseEntity.ok(HttpEntity.EMPTY);
+//    }
 
 //    //テスト用（DeleteMappingを使用するにはJavaScript側からの指定がないといけない）
 //    @GetMapping("/test")
@@ -58,4 +95,5 @@ public class PresetRestController {
 //            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 //        }
 //    }
+
 }
