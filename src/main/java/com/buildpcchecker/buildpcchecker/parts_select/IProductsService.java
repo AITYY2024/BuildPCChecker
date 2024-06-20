ab2fd829946263f961bb6bbbc232f8607010dfba
tairahoshito <yamada@xxx.com>
2024-06-20 09:35:38 +0900

    js omosiroi

--- src/main/java/com/buildpcchecker/buildpcchecker/parts_select/IProductsService.java
+++ src/main/java/com/buildpcchecker/buildpcchecker/parts_select/IProductsService.java
@@ -38,7 +38,7 @@ public interface IProductsService {
     //OSのテーブルから検索したものを表示する
     public List<OsSelectForm> searchOsParts(String os_name);
 
-    //product_idをプリセットに使うidにして送る
+    //パーツをプリセット登録する
     public Integer catchPresetInsert(PresetListForm presetListForm);
 
 }
