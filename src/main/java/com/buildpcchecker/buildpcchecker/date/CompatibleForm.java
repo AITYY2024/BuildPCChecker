b4e6f4916c80bf03e3fc194b27044fb16e18f029
yabiku <ryoga206@icloud.com>
2024-06-21 14:34:36 +0900

    commit 6/21

--- src/main/java/com/buildpcchecker/buildpcchecker/date/CompatibleForm.java
+++ src/main/java/com/buildpcchecker/buildpcchecker/date/CompatibleForm.java
@@ -1,16 +1,20 @@
 package com.buildpcchecker.buildpcchecker.date;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+
 @Data
 @AllArgsConstructor
-public class CompatibleForm {
+public class CompatibleForm { //追加、更新用Form
 
     private int compatibleId;//互換性テーブルid
 
+    @NotBlank
     private String cpuGen;//cpuの世代
 
+    @NotBlank
     private String chipset;//チップセット名
 
 }
