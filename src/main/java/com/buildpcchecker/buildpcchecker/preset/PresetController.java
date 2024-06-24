f759a0fbb113b45db3d1e57ec0cc2fd566229262
aniya <24aniya_io@axiz.co.jp>
2024-06-24 16:54:37 +0900

    互換性設定ページに追加・削除機能を実装

--- src/main/java/com/buildpcchecker/buildpcchecker/preset/PresetController.java
+++ src/main/java/com/buildpcchecker/buildpcchecker/preset/PresetController.java
@@ -13,34 +13,9 @@ public class PresetController {
     @Autowired
     PresetService presetService;
 
-
-//    @GetMapping("/test")//http://localhost:8080/test
-//    public String test(){
-//
-//        System.out.println(presetService.findAll(1));//プリセットテーブル一覧テスト
-//        System.out.println(presetService.delete(2));//プリセットテーブル削除テスト
-//        System.out.println(presetService.update());//プリセットテーブル編集テスト
-//
-//        return "test";
-//    }
-
-//    @GetMapping("/test")//http://localhost:8080/test
-//    public String test(){
-//
-////        System.out.println(presetService.findAll());//プリセットテーブル一覧テスト
-////        System.out.println(presetService.delete(1));//プリセットテーブル削除テスト
-//
-//        return "test-preset-list";
-//    }
-
-
-//    @GetMapping("/preset-list")//http://localhost:8080/test
-//    public String presetList(){
-//
-////        System.out.println(presetService.findAll());//プリセットテーブル一覧テスト
-////        System.out.println(presetService.delete(1));//プリセットテーブル削除テスト
-//
-//        return "preset-list";
-//    }
+    @GetMapping("/preset-list")
+    public String presetList(){
+        return "preset-list";
+    }
 
 }
