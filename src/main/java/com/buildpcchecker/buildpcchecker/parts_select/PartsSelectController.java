6b06ded38d13cdaaae2278ea77cab15adfd13b59
tairahoshito <yamada@xxx.com>
2024-06-25 14:51:19 +0900

    js omosiroi

---
+++ src/main/java/com/buildpcchecker/buildpcchecker/parts_select/PartsSelectController.java
@@ -0,0 +1,34 @@
+package com.buildpcchecker.buildpcchecker.parts_select;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PartsSelectController {
+
+    @Autowired
+    ProductsService productsService;
+
+    @GetMapping("/parts-select")
+    public String testparts(){
+        return "/parts-select";
+    }
+//    @PostMapping("/testPartsCategory")
+//    public String testcheck(){
+//        return "/testPartsCategory";
+//    }
+//
+//    @GetMapping("/test-parts-select")
+//    public String testpartsSelect(){
+//        return "/test-parts-select";
+//    }
+//    @PostMapping("/test-parts-select")
+//    public String testpartSelect(){
+//        return "/testPartsCategory";
+//    }
+
+
+
+}
