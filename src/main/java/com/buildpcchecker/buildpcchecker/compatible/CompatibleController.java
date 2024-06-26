0faa2b85a50e07f0bb1947c54928c2a71105eae9
yabiku <ryoga206@icloud.com>
2024-06-26 14:36:07 +0900

    commit 6/26

--- src/main/java/com/buildpcchecker/buildpcchecker/compatible/CompatibleController.java
+++ src/main/java/com/buildpcchecker/buildpcchecker/compatible/CompatibleController.java
@@ -1,5 +1,7 @@
 package com.buildpcchecker.buildpcchecker.compatible;
 
+import com.buildpcchecker.buildpcchecker.date.UsersForm;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +11,15 @@ public class CompatibleController {
     @Autowired
     CompatibleService icompatibleService;
 
+    @Autowired
+    public HttpSession session;
+
     @GetMapping("/compatible") //http://localhost:8080/compatible
     public String compatible(){
+        var session = (UsersForm)this.session.getAttribute("sessionUser");
+        if (session == null){
+            return "redirect:/login";
+        }
         return "compatible";}
 
 }
