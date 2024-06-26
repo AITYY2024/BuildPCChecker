561d3bd9d0caafb67083562e2e65f67074934b0b
yabiku <ryoga206@icloud.com>
2024-06-26 10:46:18 +0900

    commit 6/25

--- src/main/java/com/buildpcchecker/buildpcchecker/BuildpccheckerApplication.java
+++ src/main/java/com/buildpcchecker/buildpcchecker/BuildpccheckerApplication.java
@@ -1,13 +1,7 @@
 package com.buildpcchecker.buildpcchecker;
 
-import com.buildpcchecker.buildpcchecker.compatible.CompatibleDao;
-import com.buildpcchecker.buildpcchecker.compatible.IcompatibleDao;
-import com.buildpcchecker.buildpcchecker.compatible.IcompatibleService;
-import com.buildpcchecker.buildpcchecker.date.CompatibleForm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 
 @SpringBootApplication
 public class BuildpccheckerApplication {
--- src/main/java/com/buildpcchecker/buildpcchecker/compatible/CompatibleController.java
+++ src/main/java/com/buildpcchecker/buildpcchecker/compatible/CompatibleController.java
@@ -1,15 +1,13 @@
 package com.buildpcchecker.buildpcchecker.compatible;
 
-import com.buildpcchecker.buildpcchecker.date.CompatibleForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CompatibleController {
     @Autowired
-    IcompatibleService icompatibleService;
+    CompatibleService icompatibleService;
 
 //    @GetMapping("/test")//http://localhost:8080/test
 //    public String test(){
